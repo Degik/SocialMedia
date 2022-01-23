5867947ef2ba2f7b9f40b3efe895911087aca731
Degik <prodax9900@gmail.com>
2022-01-24 00:36:04 +0100

    Add files via upload

---
+++ src/socialMedia/NotifyClientInterface.java
@@ -0,0 +1,21 @@
+package socialMedia;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+public interface NotifyClientInterface extends Remote {
+	public String getClientId() throws RemoteException;
+	
+	void notifyFollowers(ArrayList<String> followersCallBack) throws RemoteException;
+	
+	void notifyPost(ArrayList<Post> postsCallBack) throws RemoteException;
+	
+	void notifyFollowing(ArrayList<String> followingCallBack) throws RemoteException;
+	
+	void notifyUsersList(ArrayList<User> usersListCallBack) throws RemoteException;
+	
+	void notifyFeedList(ArrayList<Post> usersFeedListCallBack) throws RemoteException;
+	
+	void notifyHistory(ArrayList<Double> historyCallBack) throws RemoteException;
+}
